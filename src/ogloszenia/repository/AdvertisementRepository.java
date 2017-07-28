f8f5b04971c1e65dc087bd7dc3f752a5e2e71582
Lukasz <lukbacic@gmail.com>
2017-07-28 20:37:03 +0200

    Added advertisementRepository

---
+++ src/ogloszenia/repository/AdvertisementRepository.java
@@ -0,0 +1,113 @@
+package ogloszenia.repository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+
+import ogloszenia.model.Advertisement;
+import ogloszenia.model.CATEGORY;
+import ogloszenia.model.User;
+import ogloszeniar.hibernate.util.HibernateUtil;
+
+public class AdvertisementRepository {
+	
+	public static Optional<Advertisement> findById(Integer id) {
+		Session session = null;
+		try {
+			session = HibernateUtil.openSession().getSession();
+			String hql = "SELECT e FROM Advertisement e WHERE e.id=:id";
+			Query query = session.createQuery(hql);
+			query.setParameter("id",id);
+			return Optional.ofNullable((Advertisement) query.getSingleResult());
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+			return Optional.empty();
+		} finally {
+			session.close();
+		}
+	}
+	
+	public static List<Advertisement> findByCategory(CATEGORY category){
+		Session session = null;
+		try {
+			session = HibernateUtil.openSession().getSession();
+			String hql = "SELECT e FROM Advertisement e WHERE e.category=:category";
+			Query query = session.createQuery(hql);
+			query.setParameter("category", category);
+			return query.getResultList();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+			return Collections.emptyList();
+		} finally {
+			session.close();
+		}
+	}
+
+	public static Integer persist(Advertisement advertisement) {
+		Session session = null;
+		try {
+			session = HibernateUtil.openSession().getSession();
+			session.getTransaction().begin();
+			session.persist(advertisement);
+			session.getTransaction().commit();
+			return advertisement.getId();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+			return 0;
+		} finally {
+			session.close();
+		}
+
+	}
+	
+	public static boolean merge(Advertisement advertisement) {
+		Session session = null;
+		try {
+			session = HibernateUtil.openSession().getSession();
+			session.getTransaction().begin();
+			session.merge(advertisement);
+			session.getTransaction().commit();
+			return true;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+			return false;
+		} finally {
+			session.close();
+		}
+
+	}
+	
+	public static boolean delete(Integer id) {
+		Session session = null;
+		try {
+		
+			session = HibernateUtil.openSession().getSession();
+		
+			Optional<Advertisement> advertisement = findById(id);
+			if (advertisement.isPresent()) {
+				session.getTransaction().begin();
+				advertisement.get().setIsActive(false);
+				session.merge(advertisement.get());
+				session.getTransaction().commit();
+				return true;
+			}
+			return false;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+			return false;
+		} finally {
+			session.close();
+		}
+
+	}
+
+}
