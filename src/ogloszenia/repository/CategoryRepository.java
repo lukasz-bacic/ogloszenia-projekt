b6163786ca733d3f351bcad4aac5ffb371250ea2
Lukasz <lukbacic@gmail.com>
2017-08-03 17:18:13 +0200

    products.jsp

--- src/ogloszenia/repository/CategoryRepository.java
+++ src/ogloszenia/repository/CategoryRepository.java
@@ -2,6 +2,7 @@ package ogloszenia.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import ogloszenia.model.CATEGORY;
 import ogloszenia.model.dto.CategoryDTO;
@@ -19,6 +20,13 @@ public class CategoryRepository {
 		categories.add(new CategoryDTO("Motoryzacja", "fa-car", CATEGORY.MOTORYZACJA));
 
 		return categories;
+	}
+	
+	public static CategoryDTO findByCategory(CATEGORY category) {
+		
+		return findAll().stream().
+				filter(e -> e.getCategory().equals(category)).
+				findFirst().get();
 	}
 
 }
